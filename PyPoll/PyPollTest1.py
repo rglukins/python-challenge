3611526e01aeefc3d5978b61ce460a8fa2db46e7
Rory Lukins <rglukins@gmail.com>
2018-05-09 22:19:26 -0700

    last commit 5/9

--- PyPoll/PyPollTest1.py
+++ PyPoll/PyPollTest1.py
@@ -13,13 +13,20 @@ with open(file_path, 'r', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     
     #Write code to calculate the number of votes cast
-    for voter in reader:
-        total_votes = total_votes +1
+    #for voter in reader:
+     #   total_votes = total_votes +1
 #print(total_votes)
 
-canditate_info = csv.DictReader(open(file_path, 'r'))
-for (row) in canditate_info:
+canditate_info = csv.DictReader(open(file_path, 'r'))   #create an object the reads the election data
+for (row) in canditate_info:         #identify the candidates who received votes
     can_name = row["Candidate"]
     if not can_name in candidates:
         candidates.append(can_name)
 print(candidates)
+
+votes_per_c = dict.fromkeys(candidates,0)
+print(votes_per_c)
+canditate_info = csv.DictReader(open(file_path, 'r'))
+for (row) in canditate_info:
+    if row["Candidate"] == 
+            
